942db2379eca4a1cda7b338302192076b69c577a
juwelB <91836844+juwelB@users.noreply.github.com>
2024-10-06 22:32:27 -0400

    Vulnerable commit: R07_ERR07_J

---
+++ R07_ERR07_J.java
@@ -0,0 +1,20 @@
+//Do not throw RuntimeException, Exception, or Throwable
+
+/**
+ * R07_ERR07_J
+ */
+public class R07_ERR07_J {
+
+    boolean isCapitalized(String s) {
+        if (s == null) {
+            throw new RuntimeException("Null String");
+        }
+        if (s.equals("")) {
+            return true;
+        }
+        String first = s.substring(0, 1);
+        String rest = s.substring(1);
+        return (first.equals(first.toUpperCase())
+                && rest.equals(rest.toLowerCase()));
+    }
+}
