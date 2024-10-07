87dcc2c3012f02dc347f40900726903e4489ab8d
juwelB <91836844+juwelB@users.noreply.github.com>
2024-10-06 21:44:37 -0400

    vulnerable commit: R02_XP00_J

---
+++ R02_XP00_J.java
@@ -0,0 +1,16 @@
+// Rule 02. Expressions (EXP)
+
+/**
+ * R02_XP00_J
+ */
+public class R02_XP00_J {
+
+    public void deleteFile() {
+
+        File someFile = new File("someFileName.txt");
+        // Do something with someFile
+        someFile.delete();
+
+    }
+
+}
