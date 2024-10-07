380fa8b23e58f859566f9ccfc19f94d1e7df8a63
juwelB <91836844+juwelB@users.noreply.github.com>
2024-10-06 20:54:11 -0400

    Initial comit

---
+++ cis4615.java
@@ -0,0 +1,23 @@
+
+/** ****************************************************************************
+ *  Compilation:  javac Cis4615a.java
+ *  Execution:    java Cis4615a
+ *
+ *  Prints
+ *    "Hello, World - from the class of CIS4615 - Fall 2024 - Jhuiwensley Belizaire".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4615a
+ *  Hello, World - from the class of CIS4615 - Fall 2024 - UCF
+ *  %
+ *
+ ***************************************************************************** */
+public class cis4615 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("\nHello, World - from the class of CIS4615 - Fall 2024 - Jhuiwensley Belizaire");
+    }
+
+}
