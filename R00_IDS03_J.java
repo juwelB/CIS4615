f1871f0615fd89ce8f3a2d57349e26ee83b846ad
juwelB <91836844+juwelB@users.noreply.github.com>
2024-10-06 21:40:11 -0400

    Valid commit: R00_IDS03_J

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -5,12 +5,21 @@
  */
 public class R00_IDS03_J {
 
+    public String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9_]+", username)
+        )
+      ? username:
+        "unauthorized user";
+    }
+
     public static void main(String[] args) {
+
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
 
     }
+
 }
