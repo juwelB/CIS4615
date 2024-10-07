3ad9df0360c9a82153f9b79043ff13607fbf6226
juwelB <91836844+juwelB@users.noreply.github.com>
2024-10-06 22:14:41 -0400

    Vunerable commit: R02_IDS06_J

---
+++ R02_IDS06_J.java
@@ -0,0 +1,23 @@
+//Exclude unsanitized user input from format strings
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * R02_IDS06_J
+ */
+public class R02_IDS06_J {
+
+    class Format {
+
+        static Calendar c = new GregorianCalendar(1995, GregorianCalendar.MAY, 23);
+
+        public static void main(String[] args) {
+            // args[0] should contain the credit card expiration date
+            // but might contain %1$tm, %1$te or %1$tY format specifiers
+            System.out.format(
+                    args[0] + " did not match! HINT: It was issued on %1$terd of some month", c
+            );
+        }
+    }
+}
